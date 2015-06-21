fca867f49af98a3de669ebc6a4320ef4aa6caead
Anton Staroverov <by@tonystar.me>
2015-06-21 20:16:12 +0300

    Prepare for auto-build

---
+++ make/js/bootstrap-plugins.js
@@ -0,0 +1,3 @@
+// @koala-append "../../plugins/bootstrap-ie10-viewport-hack.js"
+// @koala-append "../../plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.js"
+// @koala-append "../../plugins/bootstrap-hover-tabs/bootstrap-hover-tabs.js"
