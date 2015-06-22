fab2c7755572bc3c53d81cf65e0e837936b18f14
Anton Staroverov <by@tonystar.me>
2015-06-22 14:37:20 +0300

    Allow JS components in dropdowns by default

--- make/js/bootstrap-plugins.js
+++ make/js/bootstrap-plugins.js
@@ -1,3 +1,11 @@
 // @koala-append "../../plugins/bootstrap-ie10-viewport-hack.js"
 // @koala-append "../../plugins/bootstrap-hover-dropdown/bootstrap-hover-dropdown.js"
 // @koala-append "../../plugins/bootstrap-hover-tabs/bootstrap-hover-tabs.js"
+
+(function($) {
+  $(function() {
+    $(document).on('click', '.yamm .dropdown-menu', function(e) {
+      e.stopPropagation();
+    });
+  });
+})(jQuery);
